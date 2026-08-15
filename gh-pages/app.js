0475baf73a302ee721170f58794ee4e4acefcdc3
Ethan Morton <ethieq909@gmail.com>
2026-08-15 16:43:33 -0400

    trying different routing approach

--- gh-pages/app.js
+++ gh-pages/app.js
@@ -4,11 +4,12 @@ import {notFound} from "./pages/notFound.js";
 import {docs} from "./pages/docs.js";
 
 jsml(
-    router("/jsml/not-found", {}, {
-        "/jsml": { // gh pages puts the site at /jsml
-            "/": index(),
-            "/not-found": notFound(),
-            "/docs": docs(),
-        }
-    })
+	router("/jsml/not-found", {}, {
+		"/jsml": { // gh pages puts the site at /jsml
+			// "": index,
+			"/": index,
+			"/not-found": notFound, // actual 404 page to display
+			"/docs": docs,
+		}
+	})
 );
